e6a115590f842d65c8e791fd2b34ec8fafc11c8b
Aishwarya N <Aishwarya_N@mckinsey.com>
2022-04-02 22:55:49 +0530

    fix: add packages pg pg-hstore sequelize sequelize-cli

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const env = require("dotenv");
 
-const { healthRouter } = require("./routes/health.router");
+const { healthRouter } = require("./routes/health.routes");
+const { tweetRouter } = require("./routes/tweet.routes");
 
 env.config();
 
@@ -9,6 +10,7 @@ const port = process.env.PORT || 3001;
 const app = express();
 
 app.use("/health", healthRouter);
+app.use("/tweet", tweetRouter);
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
